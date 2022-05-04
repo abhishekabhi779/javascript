6f924314c8443e1d9f0d894466c6c30497dc014b
Abhishek <abhishekabhi779@gmail.com>
2022-05-04 16:11:24 +0530

    project2

---
+++ DOM/proj2/scripts.js
@@ -0,0 +1,11 @@
+const red = document.querySelector(".red");
+const cyan = document.querySelector(".cyan");
+const orange = document.querySelector(".orange");
+const violet = document.querySelector(".violet");
+const pink = document.querySelector(".pink");
+
+
+const center = document.querySelector(".center");
+
+
+console.log(window.getComputedStyle(red).background);
